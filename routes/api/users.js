6007d495009a8eb00b1d2c3edfd7649301451f51
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-05-15 21:23:59 +0530

    User Login (Email & Password)

--- routes/api/users.js
+++ routes/api/users.js
@@ -49,4 +49,29 @@ router.post('/register', (req, res) => {
   });
 });
 
+// @route    GET api/users/login
+// @desc     Login user / Returning JWT Token
+// @access   Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find User by email
+  User.findOne({ email }).then(user => {
+    // Check for User
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    }
+
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: 'Success' });
+      } else {
+        return res.status(400).json({ password: 'Password incorrect' });
+      }
+    });
+  });
+});
+
 module.exports = router;
