e0f407c820a56a16f1f06a2005bc231308851433
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-05-17 19:31:46 +0530

    Created API routes - add experience and education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,6 +9,12 @@ const Profile = require('../../models/Profile');
 // Load Profile validation
 const validateProfileInput = require('../../validation/profile');
 
+// Load Profile experience validation
+const validateExperienceInput = require('../../validation/experience');
+
+// Load Profile experience validation
+const validateEducationInput = require('../../validation/education');
+
 // Load User  model
 const User = require('../../models/User');
 
@@ -162,6 +168,78 @@ router.post(
         }
       })
       .catch();
+  }
+);
+
+// @route    POST api/profile/experience
+// @desc     Add experience to profile
+// @access   Private
+router.post(
+  '/experience',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        const newExp = {
+          title: req.body.title,
+          company: req.body.company,
+          location: req.body.location,
+          from: req.body.from,
+          to: req.body.to,
+          current: req.body.current,
+          description: req.body.description
+        };
+
+        // Add to experience array in profile
+        profile.experience.unshift(newExp);
+
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route    POST api/profile/education
+// @desc     Add education to profile
+// @access   Private
+router.post(
+  '/education',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateEducationInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        const newEdu = {
+          school: req.body.school,
+          degree: req.body.degree,
+          fieldOfStudy: req.body.fieldOfStudy,
+          from: req.body.from,
+          to: req.body.to,
+          current: req.body.current,
+          description: req.body.description
+        };
+
+        // Add to experience array in profile
+        profile.education.unshift(newEdu);
+
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
   }
 );
 
