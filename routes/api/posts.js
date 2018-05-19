aa374b1303d8667a468234438f793642ebbc15a4
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-05-19 21:01:45 +0530

    Created Route - Get Post(s)

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -18,6 +18,28 @@ router.get('/test', (req, res) =>
   })
 );
 
+// @route    GET api/posts
+// @desc     Get all posts
+// @access   Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ noPostsFound: 'No posts found' }));
+});
+
+// @route    GET api/posts/:id
+// @desc     Get post by ID
+// @access   Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .sort({ date: -1 })
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ noPostFound: 'No post found with that ID' })
+    );
+});
+
 // @route    POST api/posts
 // @desc     Create posts
 // @access   Private
