14404aaa06a32772c6c72c476d0e79d46df83d48
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-06-02 00:27:19 +0530

    1. postReducer - case ADD_POST 2. Post Feed link in Navbar

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,4 +1,6 @@
-const intialState = {
+import { ADD_POST } from '../actions/types';
+
+const initialState = {
   posts: [],
   post: {},
   loading: false
@@ -6,6 +8,11 @@ const intialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts]
+      };
     default:
       return state;
   }
