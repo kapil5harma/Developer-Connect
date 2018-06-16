b84150355e0a368c200e9ac0c4b852ecae721027
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-06-16 12:15:29 +0530

    Fixed chrome functioning - If Dev Tools Extension not installed on Chrome

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+// import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import rootReducer from './reducers';
+// import { createLogger } from 'redux-logger';
 
 const initialState = {};
 
+// const logger = createLogger({
+//   collapsed: true,
+//   diff: true
+// });
+
+// const middleware = [thunk, logger];
 const middleware = [thunk];
 let store;
 
@@ -14,21 +22,34 @@ function isMobileDevice() {
   );
 }
 
+// dev tools middleware
+const composeSetup =
+  process.env.NODE_ENV !== 'production' &&
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+
+// If device is running chrome browser and is not a mobile.
 if (window.navigator.userAgent.includes('Chrome') && !isMobileDevice()) {
   store = createStore(
     rootReducer,
     initialState,
-    compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    composeSetup(applyMiddleware(...middleware))
+    // composeWithDevTools(
+    //   applyMiddleware(...middleware),
+    //   window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    //     window.__REDUX_DEVTOOLS_EXTENSION__()
+    // )
   );
-} else {
+}
+// If device is running any browser other than chrome or the device is a mobile.
+else {
   store = createStore(
     rootReducer,
     initialState,
-    compose(applyMiddleware(...middleware))
+    composeSetup(applyMiddleware(...middleware))
+    // compose(applyMiddleware(...middleware))
   );
 }
 
