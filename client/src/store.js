42fbf2d2601820def4ffeed56ff6525489ecda62
Kapil Sharma <kapilsharmajmps@gmail.com>
2019-07-18 23:55:28 +0530

    build(store.js): removed production check for redux devtools

--- client/src/store.js
+++ client/src/store.js
@@ -24,7 +24,7 @@ function isMobileDevice() {
 
 // dev tools middleware
 const composeSetup =
-  process.env.NODE_ENV !== 'production' &&
+//   process.env.NODE_ENV !== 'production' &&
   typeof window === 'object' &&
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
